267610f258c556daf85870855397830b77318c6b
Ateriss <alexandralinares2729@gmail.com>
2023-05-14 19:00:01 -0500

    commit 1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "blue-title": "#6BD1FF",
+      "gray-light": "#F5F5F5",
+      "black.dark": "rgba(0, 0, 0, 0.9)",
+      "shadow-black": "rgba(0, 0, 0, 0.25)",
+      "green-title": "#00C86F",
+      "orange-tilte": "#FE8C2A",
+      "primary-blue": "#2A7AE4",
+    },
+  },
+  plugins: [],
+};
